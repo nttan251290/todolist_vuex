b16e7590020b57340a9f646223cc77e302db48c3
Nguyen Trong tan <nttan251290@gmail.com>
2019-11-09 13:51:04 +0700

    3. Bài Hiển thị danh sách Task Items (Mock data, uuid) + Part 02

---
+++ src/mocks/tasks.js
@@ -0,0 +1,24 @@
+import uuidv4 from 'uuid/v4'
+
+export default [
+  {
+    id: uuidv4(),
+    name: '	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c 1',
+    level: 0 // Small
+  },
+  {
+    id: uuidv4(),
+    name: '	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c 2',
+    level: 1 // Medium
+  },
+  {
+    id: uuidv4(),
+    name: '	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c 3',
+    level: 2 // Height
+  },
+  {
+    id: uuidv4(),
+    name: '	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ea c 4',
+    level: 0 // Small
+  }
+]
