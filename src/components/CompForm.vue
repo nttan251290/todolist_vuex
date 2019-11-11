06d3389e5e061f94917e802c8272f84ea9af54d5
Nguyen Trong Tan <tan.nt@sutrixsolutions.com>
2019-11-11 11:34:13 +0700

    4. Chức năng Form ToggleVideo + Part 02

--- src/components/CompForm.vue
+++ src/components/CompForm.vue
@@ -1,7 +1,9 @@
 <template>
   <b-col cols="12" lg="6">
     <!-- ADD : START -->
-    <form-add v-on:handleAddTask="handleAddTask" />
+    <form-add
+      v-bind:isShowForm="isShowForm"
+      v-on:handleAddTask="handleAddTask" />
     <!-- ADD : END -->
 
     <form v-if="isShowForm" action="" method="POST" class="form-inline justify-content-between">
@@ -19,7 +21,7 @@
       </div>
 
       <button type="button" class="btn btn-primary">Submit</button>
-      <button type="button" class="btn btn-secondary">Cancel</button>
+      <button v-on:click="handleCancel" type="button" class="btn btn-secondary">Cancel</button>
     </form>
   </b-col>
 </template>
@@ -48,6 +50,10 @@ export default {
     handleAddTask() {
       console.log('CompForm.vue: handleAddTask');
       this.$emit('toggleForm');
+    },
+    handleCancel() {
+      this.$emit('toggleForm');
+      // Xu ly data
     }
   }
 }
--- src/components/FormAdd.vue
+++ src/components/FormAdd.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="form-group add-task">
-    <button
+    <button 
+      v-if="!isShowForm"
       v-on:click="onClickAddTask"
       type="button" class="btn btn-info btn-block">Add Task</button>
+    <button 
+      v-else
+      v-on:click="onClickAddTask"
+      type="button" class="btn btn-primary btn-block">Close Form</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'form-add',
+  props: {
+    isShowForm: { type: Boolean, default: false }
+  },
   data() {
     return {
       
